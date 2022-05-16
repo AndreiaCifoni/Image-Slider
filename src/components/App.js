dc76a66731c00fe5570b68ed4488326593038caa
Andreia Cifoni <andreiacifoni@gmail.com>
2022-05-16 18:10:00 -0400

    added if conditions and functionality

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,23 @@ import Slider from "./Slider";
 const photos = [
   {
     id: 1,
-    photo:
-      "https://images.unsplash.com/photo-1546842931-886c185b4c8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Zmxvd2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    url: "https://images.unsplash.com/photo-1546842931-886c185b4c8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Zmxvd2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    alt: "flower 1",
   },
   {
     id: 2,
-    photo:
-      "https://images.unsplash.com/photo-1606041008023-472dfb5e530f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zmxvd2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    url: "https://images.unsplash.com/photo-1606041008023-472dfb5e530f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zmxvd2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    alt: "flower 2",
   },
   {
     id: 3,
-    photo:
-      "https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Zmxvd2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    url: "https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Zmxvd2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    alt: "flower 3",
   },
   {
     id: 4,
-    photo:
-      "https://images.unsplash.com/photo-1527061011665-3652c757a4d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGZsb3dlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    url: "https://images.unsplash.com/photo-1527061011665-3652c757a4d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGZsb3dlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    alt: "flower 4",
   },
 ];
 
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,40 +1,39 @@
 import React, { useState } from "react";
-import App from "./App";
+import "./styles.css";
 
 const Slider = ({ photos }) => {
-  const [selector, setSelector] = useState("");
-
-  const onClickPrevious = () => {
-    setSelector(selector + 1);
-  };
+  const [selector, setSelector] = useState(1);
 
   const onClickNext = () => {
-    setSelector(selector - 1);
+    if (selector < photos.length) {
+      setSelector(selector + 1);
+    }
+  };
+
+  const onClickPrevious = () => {
+    if (selector > 1) {
+      setSelector(selector - 1);
+    }
   };
 
   const eachImg = photos.map((photo) => {
-    return (
-      <img
-        key={`${photo}.id`}
-        src={`${photo}.image`}
-        width="250"
-        height="300"
-      />
-    );
+    const { id, url, alt } = photo;
+    if (id !== selector) {
+      return null;
+    }
+    return <img className="img" key={id} src={url} alt={alt} />;
   });
 
   return (
     <div>
       <div>{eachImg}</div>
-      <button onClick={onClickPrevious}>&larr; Previous</button>
-      <button onClick={onClickNext}>&rarr; Next</button>
+      {selector > 1 ? (
+        <button onClick={onClickPrevious}>&larr; Previous</button>
+      ) : null}
+      {selector < photos.length ? (
+        <button onClick={onClickNext}>&rarr; Next</button>
+      ) : null}
     </div>
   );
 };
 export default Slider;
-
-// <div>
-// <img src="{images.image}" />
-// </div>
-// <button>&larr</button>
-// <button>&rarr</button>
